62acf8d1759d68372673e7860e3b10a3ec0de9e3
tzelleke <theodros.zelleke@gmail.com>
2021-11-08 11:01:00 +0100

    feat: adds ORM models for collections/materials

--- app/pg/metadata.py
+++ app/pg/metadata.py
@@ -9,43 +9,53 @@ from sqlalchemy.dialects.postgresql import (
     TIMESTAMP,
     UUID,
 )
+from sqlalchemy.orm import declarative_base
 
 metadata = MetaData(schema="analytics_raw")
+Base = declarative_base(metadata=metadata)
 
 
-Collections = Table(
+collections = Table(
     "collections",
-    metadata,
+    Base.metadata,
     Column("id", UUID, primary_key=True),
     Column("doc", JSONB, nullable=False),
     Column("derived_at", TIMESTAMP, nullable=False),
 )
 
 
-Materials = Table(
+materials = Table(
     "materials",
-    metadata,
+    Base.metadata,
     Column("id", UUID, primary_key=True),
     Column("doc", JSONB, nullable=False),
     Column("derived_at", TIMESTAMP, nullable=False),
 )
 
 
-CollectionMaterial = Table(
-    "collection_material",
-    metadata,
-    Column(
-        "collection_id",
-        UUID,
-        ForeignKey("collections.id"),
-        primary_key=True,
-        nullable=False,
-    ),
-    Column(
-        "material_id",
-        UUID,
-        ForeignKey("materials.id"),
-        primary_key=True,
-        nullable=False,
-    ),
-)
+class Collection(Base):
+    __table__ = collections
+
+
+class Material(Base):
+    __table__ = materials
+
+
+# collection_material = Table(
+#     "collection_material",
+#     metadata,
+#     Column(
+#         "collection_id",
+#         UUID,
+#         ForeignKey("collections.id"),
+#         primary_key=True,
+#         nullable=False,
+#     ),
+#     Column(
+#         "material_id",
+#         UUID,
+#         ForeignKey("materials.id"),
+#         primary_key=True,
+#         nullable=False,
+#     ),
+# )
