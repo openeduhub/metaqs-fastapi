2ca26a8f64a3ff270615407eff961b4e31a831e7
tzelleke <theodros.zelleke@gmail.com>
2021-11-13 22:22:58 +0100

    feat: adds None to 0 (int) validator

--- app/models/util.py
+++ app/models/util.py
@@ -3,6 +3,12 @@ from typing import Optional
 from pydantic.validators import str_validator
 
 
+def none_to_zero(v: int) -> Optional[int]:
+    if v is None:
+        return 0
+    return v
+
+
 def empty_to_none(v: str) -> Optional[str]:
     if v == "":
         return None
