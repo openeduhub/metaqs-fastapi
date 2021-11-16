c7142b10d1be40f696ada6d0b90ffb2b0ee220ae
tzelleke <theodros.zelleke@gmail.com>
2021-11-16 14:42:50 +0100

    feat: declares custom enum types ResourceType, ResourceField

--- app/pg/metadata.py
+++ app/pg/metadata.py
@@ -1,4 +1,7 @@
+from enum import Enum, unique
+
 import sqlalchemy as sa
+import sqlalchemy.dialects.postgresql as pgsql
 from sqlalchemy.orm import declarative_base
 
 from app.core.config import DATABASE_URL
@@ -19,10 +22,34 @@ class Material(Base):
     )
 
 
+@unique
+class ResourceType(Enum):
+    COLLECTION = 'collection'
+    MATERIAL = 'material'
+
+
+@unique
+class ResourceField(Enum):
+    TITLE = 'title'
+    DESCRIPTION = 'description'
+    KEYWORDS = 'keywords'
+    EDU_CONTEXT = 'edu_context'
+    TAXON_ID = 'taxon_id'
+    LEARNING_RESOURCE_TYPE = 'learning_resource_type'
+    LICENSE = 'license'
+    ADS_QUALIFIER = 'ads_qualifier'
+    OBJECT_TYPE = 'object_type'
+    INTENDED_ENDUSER_ROLE = 'intended_enduser_role'
+    URL = 'url'
+    REPLICATION_SOURCE = 'replication_source'
+    REPLICATION_SOURCE_ID = 'replication_source_id'
+
+
 spellcheck_queue = sa.Table(
     "spellcheck_queue",
     Base.metadata,
-    sa.PrimaryKeyConstraint("resource_id", "resource_field"),
+    sa.Column('resource_field', pgsql.ENUM(ResourceField), primary_key=True, nullable=False),
+    sa.Column('resource_type', pgsql.ENUM(ResourceType), nullable=False),
     schema="staging",
     autoload_with=engine,
 )
@@ -31,7 +58,8 @@ spellcheck_queue = sa.Table(
 spellcheck = sa.Table(
     "spellcheck",
     Base.metadata,
-    sa.PrimaryKeyConstraint("resource_id", "resource_field"),
+    sa.Column('resource_field', pgsql.ENUM(ResourceField), primary_key=True, nullable=False),
+    sa.Column('resource_type', pgsql.ENUM(ResourceType), nullable=False),
     schema="store",
     autoload_with=engine,
 )
