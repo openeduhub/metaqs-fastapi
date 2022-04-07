90f48fe1cb64838f05c75819ab1a94c0325eadf2
Robert Meissner <robert.meissner@codecentric.de>
2022-04-07 15:26:02 +0200

    Code exploration
    WIP

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -4,7 +4,10 @@ from app.core.config import API_VERSION
 
 from .util import PaginationParams, materials_filter_params, pagination_params
 
-api = import_module(f".{API_VERSION}", package=__name__)
+print(f"Current API_VERSION: {API_VERSION}")
+if API_VERSION == "v1" or API_VERSION == "":
+    from .v1 import real_time_router
+    from .v1 import analytics_router
 
-real_time_router = api.real_time_router
-analytics_router = api.analytics_router
+real_time_router = real_time_router
+analytics_router = analytics_router
